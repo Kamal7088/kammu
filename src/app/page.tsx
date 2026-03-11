7d5268a568c6913e647d0c66a87ce86eee78ddcd
Kamal7088 <kamalsinghb57@gmail.com>
2026-03-12 00:32:42 +0530

    commit update v

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen bg-gray-900">
       <h1 className="text-4xl font-bold text-white">
         Next.js + Tailwind Working 🚀
-
+shshhsh
         kamalkbn
         {/* kkksksdjjjd */}
       </h1>
