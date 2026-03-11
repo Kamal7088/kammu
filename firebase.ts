5e164f232b25736484ea72f4ce7686aef8c6b19f
Kamal7088 <kamalsinghb57@gmail.com>
2026-03-12 00:31:59 +0530

    jdj

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +16,8 @@ const firebaseConfig = {
   appId: "1:601663175328:web:3224ef48f49a420977a7c5",
   measurementId: "G-X5JVM8WFP7"
 };
-
+// hdhdh
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900">
       <h1 className="text-4xl font-bold text-white">
         Next.js + Tailwind Working 🚀
+
+        kamalkbn
+        {/* kkksksdjjjd */}
       </h1>
     </div>
   );
